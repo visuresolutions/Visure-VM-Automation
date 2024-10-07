55ce8a9fe8b270d932e4498eaa5f303b5a3d6d02
visuresolutions <helpdesk@visuresolutions.com>
2024-10-07 17:25:30 -0400

    First Version of Visure Install Files

---
+++ InstallPostgres.ps1
@@ -0,0 +1,21 @@
+﻿# File: install-postgresql.ps1
+
+# Update and install Chocolatey package manager
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+
+# Install PostgreSQL 16
+choco install postgresql --version=16.0 -y
+
+# Add PostgreSQL to path (assumes default installation path)
+$env:Path += ";C:\Program Files\PostgreSQL\16\bin"
+
+# Start PostgreSQL service
+Start-Service -Name "postgresql-x64-16"
+
+# Create Database and Tablespaces
+$psqlPath = "C:\Program Files\PostgreSQL\16\bin\psql.exe"
+& $psqlPath -U postgres -c "CREATE DATABASE VisureDB WITH ENCODING 'WIN1252';"
+& $psqlPath -U postgres -c "CREATE TABLESPACE VisureDB_DATA OWNER postgres LOCATION 'C:\path_to_data';"
+& $psqlPath -U postgres -c "CREATE TABLESPACE VisureDB_IDX OWNER postgres LOCATION 'C:\path_to_idx';"
+& $psqlPath -U postgres -c "CREATE SCHEMA VisureDB_Schema AUTHORIZATION postgres;"
---
+++ VisureInstall.ps1
@@ -0,0 +1,41 @@
+﻿Invoke-WebRequest https://storage.googleapis.com/visure_downloads/Products/Visure%20Requirements/v$env:version/VisureRequirementsALMServer_$env:version.msi -Outfile c:\Temp\server.msi; \
+Invoke-WebRequest https://storage.googleapis.com/visure_downloads/Products/Visure%20Requirements/v$env:version/VisureAuthoring_$env:version.msi -Outfile c:\Temp\authoring.msi; \
+Start-Process -FilePath "C:\Temp\python.exe" -ArgumentList "/quiet", "PrependPath=1", "InstallAllUsers=1", "Include_launcher=1" -Wait -NoNewWindow; \
+Start-Process -FilePath "C:\Temp\dotnetruntime.exe" -ArgumentList "/install", "/passive"  -NoNewWindow -Wait; \
+Start-Process -FilePath "C:\Temp\vcredist2022_x64.exe" -ArgumentList "/install", "/quiet", "/norestart" -NoNewWindow -Wait; \
+Start-Process msiexec -ArgumentList '/i "C:\Temp\server.msi"', '/qn', '/norestart', '/L*v "C:\Temp\install_log.txt"' -NoNewWindow -Wait; \
+Start-Process -FilePath "C:\Temp\aspnetcore64.exe" -ArgumentList "/install", "/passive"  -NoNewWindow -Wait; \
+Start-Process -FilePath "C:\Temp\aspnetcore86.exe" -ArgumentList "/install", "/passive"  -NoNewWindow -Wait; \
+Start-Process -FilePath "C:\Temp\hosting8.exe" -ArgumentList "/install", "/quiet", "/norestart" -NoNewWindow -Wait; \
+Start-Process msiexec -ArgumentList '/i "C:\Temp\msodbcsql.msi"', 'IACCEPTMSODBCSQLLICENSETERMS=YES', '/qn', '/norestart' -NoNewWindow -Wait; \
+Start-Process msiexec -ArgumentList '/i "C:\Temp\psqlodbc_x64.msi"', '/qb' -NoNewWindow -Wait;
+Copy-Item C:\Temp\VRSettings.json -Destination 'C:\Program Files\Visure Solutions, Inc\Visure Server 8'; \
+Copy-Item C:\Temp\server.crt -Destination 'C:\Program Files\Visure Solutions, Inc\Visure Server 8'; \
+Copy-Item C:\Temp\server.key -Destination 'C:\Program Files\Visure Solutions, Inc\Visure Server 8'; \
+(Get-Content -Path "VRSettings.json" -Raw) -replace 'SERVER_NAME', $env:SERVER | Set-Content -Path "VRSettings.json"; \
+(Get-Content -Path "VRSettings.json" -Raw) -replace 'DATABASE_NAME', $env:DATABASE | Set-Content -Path "VRSettings.json"; \
+(Get-Content -Path "VRSettings.json" -Raw) -replace 'USER_NAME', $env:USER | Set-Content -Path "VRSettings.json"; \
+$VariableName = .\VisureToken.exe $($env:PASSW); \
+$VariableName2= ($VariableName -split '\\n' | Select-Object -Skip 16) -join '\\n'; \
+(Get-Content -Path "VRSettings.json" -Raw) -replace 'PASSWORD_NAME', $VariableName2 | Set-Content -Path "VRSettings.json"; \
+$VariableName3 = .\VisureToken.exe $($env:APASSW); \
+$VariableName4= ($VariableName3 -split '\\n' | Select-Object -Skip 16) -join '\\n'; \
+(Get-Content -Path "VRSettings.json" -Raw) -replace 'ADMIN_PASSWORD', $VariableName4 | Set-Content -Path "VRSettings.json"; \
+Install-WindowsFeature -name Web-Server -IncludeManagementTools;
+$thumbprint = 'D9158BACC1BD91AFEDF585EB02703AAAD59C88AC'; \
+New-WebBinding -Name 'Default Web Site' -Protocol https -Port 443 -SslFlags 0; \
+$binding = Get-WebBinding -Name 'Default Web Site' -Protocol https; \
+if ($null -eq $binding) { Write-Host "Binding not found"; exit 1 }; \
+$cert = Get-ChildItem -Path cert:\LocalMachine\My\$thumbprint; \
+if ($null -eq $cert) { Write-Host "Certificate not found"; exit 1 }; \
+$hash = $cert.GetCertHashString(); \
+$store = 'my'; \
+$binding.AddSslCertificate($hash, $store);
+Set-Location 'C:\inetpub\Visure Authoring 8'; \
+(Get-Content -Path "appsettings.json" -Raw) -replace 'container', $env:DATABASE | Set-Content -Path "appsettings.json"; \
+Set-Location 'C:\inetpub\Visure Authoring 8\wwwroot\assets'; \
+(Get-Content -Path "settings.json" -Raw) -replace 'container', $env:DATABASE | Set-Content -Path "settings.json"; \
+Copy-Item C:\Temp\visurecloud.com.crt -Destination 'C:\inetpub\Visure Authoring 8\Certificates'; \
+IISRESET; \
+Set-Location 'C:\Temp'; \
+Remove-Item -Recurse -Path '*';
